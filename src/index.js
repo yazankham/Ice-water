fb0d677b3261e4083c6575b06ea89c7232db2527
yazan khamayseh <201122@ppu.edu.ps>
2025-08-17 12:23:13 +0300

    Clean up project: Remove duplicate and unused files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React      from 'react';
 import ReactDOM   from 'react-dom';
 import './index.css';
 import 'tw-elements';
-import  App       from './components/App';
+import  App       from './App';
 
 ReactDOM.render(
   <React.StrictMode>
